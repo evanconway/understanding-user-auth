793493afd8381e34e65d7d56a68236c5421c9529
Evan Conway <evanconwaymusic@gmail.com>
2023-09-28 17:40:56 -0400

    add certificate

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,15 @@
 import https from "https";
+import fs from "fs";
 
-const server = https.createServer((req, res) => {
+const options = {
+    key: fs.readFileSync("../certificate/server.key"),
+    cert: fs.readFileSync("../certificate/server.cert"),
+};
+
+const server = https.createServer(options, (req, res) => {
     res.write("hello world");
     res.end();
 });
+
 const port = 8080;
 server.listen(port, () => console.log(`server listening on port ${port}`));
