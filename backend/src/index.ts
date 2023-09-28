0de5924fb453c550d1078cf8adf8c686c656cfd5
Evan Conway <evanconwaymusic@gmail.com>
2023-09-28 15:40:50 -0400

    add frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,5 @@
-console.log("hello world");
-console.log("welcome to RC");
+import https from "https";
+
+const server = https.createServer();
+const port = 8080;
+server.listen(port, () => console.log(`server listening on port ${port}`));
