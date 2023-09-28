e2f71145967daaa25eae3ab9aae1eae58482146a
Evan Conway <evanconwaymusic@gmail.com>
2023-09-28 15:58:08 -0400

    add certificate

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,8 @@
 import https from "https";
 
-const server = https.createServer();
+const server = https.createServer((req, res) => {
+    res.write("hello world");
+    res.end();
+});
 const port = 8080;
 server.listen(port, () => console.log(`server listening on port ${port}`));
